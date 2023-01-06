01bcc8013cc2c73dc535a30c95b8e0beed4d843b
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-06 16:35:20 -0700

    Declare func

---
+++ two_sum.py
@@ -0,0 +1,12 @@
+"""
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+"""
+
+
+def two_sum(arr: list, target: int) -> list:
+    pass
+
+
+if __name__ == "__main__":
+    pass
