0a346190bd90a17b7ecb544f4a68ec9ae24aa838
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-07 23:47:59 -0700

    Brute Force Method

--- two_sum.py
+++ two_sum.py
@@ -5,8 +5,13 @@ return indices of the two numbers such that they add up to target.
 
 
 def two_sum(arr: list, target: int) -> list:
-    pass
+    size_of_arr = len(arr)
+    for i in range(0, size_of_arr):
+        for j in range(i + 1, size_of_arr):
+            if (arr[i] + arr[j]) == target:
+                return [i, j]
+    return []
 
 
 if __name__ == "__main__":
-    pass
+    assert two_sum([1, 2, 3, 4, 5], 8) == [2, 4]
