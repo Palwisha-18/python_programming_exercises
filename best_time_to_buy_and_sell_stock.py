10283174d715ed48c1df53607ac4fbe033657e82
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-09 14:01:27 -0700

    Declare brute force function

---
+++ best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,14 @@
+"""
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and
+choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+"""
+
+
+def max_profit_brute_force(arr: list) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    assert max_profit_brute_force([7, 1, 5, 3, 6, 4]) == 5
