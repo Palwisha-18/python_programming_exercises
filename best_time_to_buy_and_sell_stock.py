b1f25f7e6eeadc2f16fef8fddfdf2c581fc5e59e
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-10 12:46:45 -0700

    One pass approach for finding max profit in buying and selling stocks added

--- best_time_to_buy_and_sell_stock.py
+++ best_time_to_buy_and_sell_stock.py
@@ -15,6 +15,22 @@ def max_profit_brute_force(arr: list) -> int:
     return max_profit
 
 
+def max_profit_one_pass(arr: list) -> int:
+    min_buying_price = 100000000
+    max_profit = 0
+    for i in range(0, len(arr) - 1):
+        if arr[i] < min_buying_price:
+            min_buying_price = arr[i]
+
+        if arr[i + 1] - min_buying_price > max_profit:
+            max_profit = arr[i + 1] - min_buying_price
+
+    return max_profit
+
+
 if __name__ == "__main__":
     assert max_profit_brute_force([7, 1, 5, 3, 6, 4]) == 5
     assert max_profit_brute_force([7, 6, 4, 3, 1]) == 0
+
+    assert max_profit_one_pass([7, 1, 5, 3, 6, 4]) == 5
+    assert max_profit_one_pass([7, 6, 4, 3, 1]) == 0
