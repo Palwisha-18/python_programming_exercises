39627eb25870951774039db0062fd4bb77be99d3
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-04-20 13:54:40 -0600

    Implement Find Common Chars using Counter

--- find_common_chars.py
+++ find_common_chars.py
@@ -1,4 +1,20 @@
 """
 Given a string array words, return an array of all characters that show up in all strings within the words
 (including duplicates). You may return the answer in any order.
+
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
 """
+from collections import Counter
+
+
+def find_common_chars_using_counter(arr):
+    result = Counter(arr[0])
+    for word in arr:
+        result &= Counter(word)
+    return list(result.elements())
+
+
+if __name__ == "__main__":
+    assert find_common_chars_using_counter(["bella", "label", "roller"]) == ["e", "l", "l"]
+    assert find_common_chars_using_counter(["cool", "lock", "cook"]) == ["c","o"]
